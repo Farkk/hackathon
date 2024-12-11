cbe50cb90b75e9bfa032485fd3c7afe47ee9d44b
vengord <artomkolomeec@gmail.com>
2024-12-11 14:18:09 +0600

    initial

---
+++ backend/app/db.py
@@ -0,0 +1,4 @@
+fake_db = [
+    {"id": 1, "name": "Item 1", "description": "Description 1", "price": 10.0, "on_offer": False},
+    {"id": 2, "name": "Item 2", "description": "Description 2", "price": 20.0, "on_offer": True},
+]
